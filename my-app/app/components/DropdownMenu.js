3dd421c53432cccdd56364b49dbd53827e0bc202
Florian Lüers <flo.lueers+github@gmail.com>
2024-11-22 17:06:58 +0100

    (~) DropdownMenu: Paar Verbesserungen

--- my-app/app/components/DropdownMenu.js
+++ my-app/app/components/DropdownMenu.js
@@ -10,24 +10,24 @@ export default function DropdownMenu () {
     let availableOptions = [{label: "vegan", icon: "🌱"}, {label: "vegetarisch", icon: "🥛"}, {label: "fleisch", icon: "🥩"} ]
     const [selectedOption, setSelectedOption] = useState(0)
 
-    
-    const handleChange = (item) => {
-        setSelectedOption(item.target.value)
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value)
     }
 
     return (
         <>
-            {selectedOption === 0 ? (availableOptions[0].icon) : selectedOption === 1 ? (availableOptions[1].icon) : (availableOptions[2].icon)}
+            {availableOptions[selectedOption].icon}
 
             <Select
                 size="small"
-                defaultValue={0}
+                defaultValue={selectedOption}
                 variant="standard"
                 onChange={handleChange}
                 sx={{ ml: 1, my: 0.5 }}
             >
                 {availableOptions.map((item, index) => (
-                    <MenuItem key={index} value={index}>
+                    <MenuItem key={item.label} value={index}>
                         {item.label}
                     </MenuItem>
                 ))}
