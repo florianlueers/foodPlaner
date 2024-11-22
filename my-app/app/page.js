e2db323ac4428fc1388945eb41534dee51d55904
Florian Lüers <flo.lueers+github@gmail.com>
2024-11-22 17:02:59 +0100

    (+): first commit

---
+++ my-app/app/page.js
@@ -0,0 +1,8 @@
+import DropdownMenu from "./components/DropdownMenu";
+import InfiniteListComponent from "./components/InfiniteListComponent";
+
+export default function Home() {
+  return (
+    <DropdownMenu />
+  );
+}
